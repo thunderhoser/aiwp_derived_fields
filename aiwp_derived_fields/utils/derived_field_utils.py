da039ebe28c7bf9d8a61ea52a02a484c2a4a9bb4
thunderhoser <lagerqui@ualberta.ca>
2024-03-08 14:24:27 -0700

    FOO

--- aiwp_derived_fields/utils/derived_field_utils.py
+++ aiwp_derived_fields/utils/derived_field_utils.py
@@ -1806,6 +1806,15 @@ def get_precipitable_water(
     print(pressure_matrix_pascals[:, row_index, column_index])
     print(spec_humidity_matrix_kg_kg01[:, row_index, column_index])
 
+    inds = numpy.argsort(-pressure_matrix_pascals[:, row_index, column_index])
+    subinds = numpy.where(numpy.invert(
+        numpy.isnan(spec_humidity_matrix_kg_kg01[:, row_index, column_index][inds])
+    ))[0]
+
+    print(pressure_matrix_pascals[:, row_index, column_index][inds][subinds])
+    print(spec_humidity_matrix_kg_kg01[:, row_index, column_index][inds][subinds])
+    print(precipitable_water_matrix_kg_m02[row_index, column_index])
+
     assert not numpy.any(precipitable_water_matrix_kg_m02 < 0.)
 
     print('Estimating precipitable water took {0:.1f} seconds.'.format(
